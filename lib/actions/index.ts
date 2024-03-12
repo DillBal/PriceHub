67b1b9e8d6cbdcbe014031fcbba9e68993720e2d
DillBall <baldwidi@oregonstate.edu>
2024-03-12 16:13:13 -0700

    Finishing up the project

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,8 @@ import Product from "../models/product.model";
 import { scrapeAmazonProduct } from "../scraper/index";
 import { connectToDatabase } from "../scraper/mongoose";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
+import { generateEmailMessage, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     //scrape the product page
@@ -63,6 +65,52 @@ export async function getProductById(productId: string) {
       if(!product) return null;
   
       return product;
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+export async function getAllProducts() {
+    try {
+        connectToDatabase();
+        const products = await Product.find();
+        return products;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getSimilarProducts(productId: string) {
+    try {
+        connectToDatabase();
+        const currentProduct = await Product.findById(productId);
+        if(!currentProduct) return null;
+        const similarProducts= await Product.find({ 
+            _id: { $ne: productId }, 
+        }).limit(3);
+        return similarProducts;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+    try {
+      const product = await Product.findById(productId);
+  
+      if(!product) return;
+  
+      const userExists = product.users.some((user: User) => user.email === userEmail);
+  
+      if(!userExists) {
+        product.users.push({ email: userEmail });
+  
+        await product.save();
+  
+        const emailContent = await generateEmailMessage(product, "WELCOME");
+  
+        await sendEmail(emailContent, [userEmail]);
+      }
     } catch (error) {
       console.log(error);
     }
